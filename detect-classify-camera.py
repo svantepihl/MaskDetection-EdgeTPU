a8a5c172d392c1a61d528d54e723c08f75416413
Svante Pihl <hej@svantepihl.com>
2021-05-25 20:02:02 +0000

    add fps

--- detect-classify-camera.py
+++ detect-classify-camera.py
@@ -8,6 +8,7 @@ from pycoral.utils.dataset import read_label_file
 import argparse
 import cv2
 import os
+import time
 
 def detect_and_classify_faces(detector,classifier,image,threshold,padding = 10):
     predictions = []
@@ -95,7 +96,7 @@ def main():
                         default=os.path.join(default_model_dir, default_labels))
     parser.add_argument('--camera_idx', type=int,
                         help='Index of which video source to use. ', default=0)
-    parser.add_argument('--threshold', type=float, default=0.3,
+    parser.add_argument('--threshold', type=float, default=0.1,
                         help='classifier score threshold')
     args = parser.parse_args()
 
@@ -107,20 +108,46 @@ def main():
     classification_interpreter = make_interpreter(args.classification_model, device=':0')
     classification_interpreter.allocate_tensors()
 
-    print('Starting camera {}...'.format(args.image))
+    print('Starting camera {}...'.format(args.camera_idx))
     cap = cv2.VideoCapture(args.camera_idx)
 
+    # used to record the time when we processed last frame
+    prev_frame_time = 0
+    
+    # used to record the time at which we processed current frame
+    new_frame_time = 0
+
     while cap.isOpened():
+        start = time.perf_counter()
         ret, frame = cap.read()
         if not ret:
             break
-        cv2_img = frame
+        image = frame
 
-        image = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        #image = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
 
         boxes,predictions = detect_and_classify_faces(detection_interpreter,classification_interpreter,image,args.threshold)
 
         final_image = append_boxes_to_img(image,boxes,predictions)
+
+        frame_time = time.perf_counter() - start
+
+            # Calculating the fps
+  
+        # fps will be number of frame processed in given time frame
+        # since their will be most of time error of 0.001 second
+        # we will be subtracting it to get more accurate result
+        fps = 1/frame_time
+    
+        # converting the fps into integer
+        fps = int(fps)
+    
+        # converting the fps to string so that we can display it on frame
+        # by using putText function
+        fps = str(fps)
+    
+        # puting the FPS count on the frame
+        cv2.putText(final_image, fps, (2, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3, cv2.LINE_AA)
 
         cv2.imshow('frame', final_image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -128,8 +155,6 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
-
-
 
 if __name__ == '__main__':
     main()
